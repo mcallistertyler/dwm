75a86d74110cdc0bf7a603a53e43e99190034a00
Tyler McAllister <tyler.mcallister@airthings.com>
2019-11-29 15:57:35 +0100

    Layouts are cool.

--- config.h
+++ config.h
@@ -41,11 +41,15 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
+#include "fibonacci.c"
+#include "layouts.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "tile",      tile },    /* first entry is default */
-	{ "float",      NULL },    /* no layout function means floating behavior */
+	{ "float",     NULL },    /* no layout function means floating behavior */
 	{ "zoom",      monocle },
+	{ "spiral",       spiral },
+	{ "dwindle",      dwindle },
 };
 
 /* key definitions */
@@ -82,6 +86,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,			XK_q,	   setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
