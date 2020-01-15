a83527d7830de65fb5529a915c8a6cc3b8115ba9
Tyler <tytheguy95@gmail.com>
2020-01-15 19:20:51 +0100

    i forget what changed lol i'm sure it's nothing too important lel

--- config.h
+++ config.h
@@ -19,7 +19,8 @@ static const char col_pastelblue[] = "#88ddf0";
 static const char col_black[]	    = "#000000";
 //static const unsigned int baralpha = 0xd0;
 static const unsigned int baralpha = 0xc8;
-static const unsigned int borderalpha = OPAQUE;
+static const unsigned int borderalpha = 0xc8;
+//static const unsigned int borderalpha = OPAQUE;
 //static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 //	[SchemeNorm] = { col_black, col_pastelpink, col_pastelblue },
