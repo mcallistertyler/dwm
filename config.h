7aaa232f9dbc7821b14d81b9b82bb36c4cc93317
Tyler <tytheguy95@gmail.com>
2020-01-16 21:01:58 +0100

    Systray working but hmmm weird black background.

--- config.h
+++ config.h
@@ -4,6 +4,10 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx	= 6;		/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 1;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]	    = { "Terminus (TTF):size=9" }; 
@@ -17,7 +21,6 @@ static const char col_pastelpink[]  = "#ffcccc";
 //static const char col_pastelblue[]  = "#d1fff4";
 static const char col_pastelblue[] = "#88ddf0";
 static const char col_black[]	    = "#000000";
-//static const unsigned int baralpha = 0xd0;
 static const unsigned int baralpha = 0xc8;
 static const unsigned int borderalpha = 0xc8;
 //static const unsigned int borderalpha = OPAQUE;
@@ -74,9 +77,9 @@ static const Layout layouts[] = {
 	{ "float",		NULL },    /* no layout function means floating behavior */
 	{ "zoom",		monocle },
 	{ "spiral",		spiral },
-	{ "dwindle",		dwindle },
-	{ "cmaster",		centeredmaster },
-	{ "cfmaster",		centeredfloatingmaster },
+	{ "dwindle",	dwindle },
+	{ "cmaster",	centeredmaster },
+	{ "cfmaster",	centeredfloatingmaster },
 	{ "grid",		grid },
 	{ NULL,			NULL },
 };
@@ -98,7 +101,6 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 // static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = { "rofi", "-show", "run", "-display-run", ">>> ", NULL};
 static const char *termcmd[]  = { "st", NULL };
-// Not currently working, needs a super cool "im definitely not a cunt" terminal like st
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
